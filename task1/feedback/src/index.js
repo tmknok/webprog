7e579f893624bf8efda30ff3e667689381a34d7e
tmknok <tiinan.m@gmail.com>
2019-02-28 23:44:41 +0200

    task1

---
+++ task1/feedback/src/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+)
+const Statistics = ({ good, neutral, bad, avg, per }) => (
+    <div>
+        <Statistic counter={good} text="Good" />
+        <Statistic counter={neutral} text="Neutral" />
+        <Statistic counter={bad} text="Bad" />
+        <Statistic counter={avg} text="Average" />
+        <Statistic counter={per} text="Positive percentage" />
+    </div>
+)
+
+const Statistic = (props) => (
+    <div>
+        <p>{props.text} {props.counter}</p>
+    </div>
+)
+
+class App extends React.Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+    }
+    goodClick() {
+        this.setState({ good: this.state.good + 1 })
+    }
+
+    neutralClick() {
+        this.setState({ neutral: this.state.neutral + 1 })
+    }
+
+    badClick() {
+        this.setState({ bad: this.state.bad + 1 })
+    }
+    render() {
+        const { good, neutral, bad } = this.state
+        const avg = ((good - bad) / (good + neutral + bad) || 0).toFixed(2)
+        const per = (good / (good + neutral + bad) || 0).toFixed(2)
+        return (
+
+            < div className='konsta'>
+                <h1>Give feedback</h1>
+                <Button handleClick={this.goodClick.bind(this)} text="Good" />
+                <Button handleClick={this.neutralClick.bind(this)} text="Neutral" />
+                <Button handleClick={this.badClick.bind(this)} text="Bad" />
+                <h2>Statistics</h2>
+                <Statistics good={good} neutral={neutral} bad={bad} avg={avg} per={per} />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ task1/task1/src/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part.name}, {props.part.exercises}
+        </p>
+    )
+}
+
+const Header = (props) => {
+    return (
+        <h1>{props.course.name}</h1>
+    )
+}
+
+const Contents = (props) => {
+    return (
+        <div>
+            <Part part={props.parts[0]} />
+            <Part part={props.parts[1]} />
+            <Part part={props.parts[2]} />
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <div>
+            <p>Total of {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} exercixes</p>
+        </div>
+    )
+}
+const App = () => {
+    const course = {
+        name: 'Superadvanced web and mobile programming',
+        parts: [
+            {
+                name: 'Basic of React',
+                exercises: 8
+            },
+            {
+                name: 'Using props',
+                exercises: 10
+            },
+            {
+                name: 'Component states',
+                exercises: 12
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <Header course={course} />
+            <Contents parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
